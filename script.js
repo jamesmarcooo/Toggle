5a9555ddffd6e17e70be2822316d528731c6fb7c
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-03 21:35:32 +0800

    Added some comments

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-$(‘#main’).toggleClass(localStorage.toggled);
+HTML CSS JSResult Skip Results Iframe
+EDIT ON
+
+$('#main').toggleClass(localStorage.toggled);
 
 function darkLight() {
-    if (localStorage.toggled != 'dark') {
-       $('#main, p').toggleClass('dark', true);
-       localStorage.toggled = "dark";
-    } else {
-       $('#main, p').toggleClass('dark', false);
-       localStorage.toggled = "";
-    }
- }
-
- if ($('main').hasClass('dark')) {
-    $( '#checkBox' ).prop( "checked", true )
-} else {
-    $( '#checkBox' ).prop( "checked", false )
+  /*DARK CLASS*/
+  if (localStorage.toggled != 'dark') {
+    $('#main, p').toggleClass('dark', true);
+    localStorage.toggled = "dark";
+     
+  } else {
+    $('#main, p').toggleClass('dark', false);
+    localStorage.toggled = "";
+  }
 }
+
+/*Add 'checked' property to input if background == dark*/
+if ($('main').hasClass('dark')) {
+   $( '#checkBox' ).prop( "checked", true )
+} else {
+  $( '#checkBox' ).prop( "checked", false )
+}
+
+Run Pen
+
+Resources1× 0.5× 0.25×Rerun
